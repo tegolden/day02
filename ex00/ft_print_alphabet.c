3c3bb67344b3fccde344bec2ac977c542e141dde
Teresho Golden <tegolden@c4r22s1.wethinkcode.co.za>
2020-07-08 14:37:50 +0200

    first c prg

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_print_alphabet(void)
+{
+	char i;
+
+	i = 'a';
+	while(i <= 'z')
+	{
+		ft_putchar(i);
+		ft_putchar('\0');
+		i++;
+	}
+}
